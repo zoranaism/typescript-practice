cf01d7e2c82eacc88b56ddf2cf5d57a1455c2f7e
zoranaism <lazareviczorana@gmail.com>
2020-06-23 12:08:56 +0200

    Literal type

--- app.ts
+++ app.ts
@@ -1,16 +1,26 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resultType: string
+) {
   let result;
   if (typeof input1 === "number" && typeof input2 === "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+  if (resultType === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combinedAges = combine(30, 26);
+const combinedAges = combine(30, 26, "as-number");
 console.log(combinedAges);
 
-const combinedName = combine("max", "ana");
-console.log(combinedName);
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
 
+const combinedName = combine("max", "ana", "as-text");
+console.log(combinedName);
